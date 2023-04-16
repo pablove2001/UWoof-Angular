5bd5d8a0f209d7c20d6526d2a137a129c250901c
Pablo Vergara <vergarapablo2001@gmail.com>
2023-04-16 14:49:21 -0600

    create top-bar

--- src/app/layouts/top-bar/top-bar.component.ts
+++ src/app/layouts/top-bar/top-bar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.scss']
 })
-export class TopBarComponent {
+export class TopBarComponent implements OnInit {
 
+  ngOnInit(): void {
+    const menuToggle = document.querySelector(".menu-toggle");
+    if (menuToggle !== null) {
+      menuToggle.addEventListener("click", function () {
+        const nav = document.querySelector(".nav");
+        if (nav !== null) {
+          nav.classList.toggle("mobile-nav");
+          menuToggle.classList.toggle("is-active");
+        }
+      });
+    }
+  }
 }
