f4e540d51f06898d0602d8ba2b0a6b63dbbe9e9a
Pablo Vergara <vergarapablo2001@gmail.com>
2023-04-16 09:26:34 -0600

    create project and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatsComponent } from './pages/chats/chats.component';
+import { CreatePostComponent } from './pages/create-post/create-post.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PostsComponent } from './pages/posts/posts.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'chats', component: ChatsComponent },
+  { path: 'create-post', component: CreatePostComponent },
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'posts', component: PostsComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'sign-up', component: SignUpComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ChatsComponent } from './pages/chats/chats.component';
+import { CreatePostComponent } from './pages/create-post/create-post.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PostsComponent } from './pages/posts/posts.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { TopBarComponent } from './layouts/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ChatsComponent,
+    CreatePostComponent,
+    LoginComponent,
+    PostsComponent,
+    ProfileComponent,
+    SignUpComponent,
+    FooterComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
