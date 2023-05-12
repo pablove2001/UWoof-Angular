abf1265e3dfbe981574bfbaa36ed13c72ce092c5
Asis Vazquez <aasisvc2000@gmail.com>
2023-05-12 16:32:41 -0600

    comienzos de sockets chat, boton posts y archivos

--- src/app/pages/chats/chats.component.ts
+++ src/app/pages/chats/chats.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { io } from 'socket.io-client';
+import { environment } from 'src/enviroments/enviroment';
 
 @Component({
   selector: 'app-chats',
   templateUrl: './chats.component.html',
   styleUrls: ['./chats.component.scss']
 })
-export class ChatsComponent {
+export class ChatsComponent implements OnInit{
+  messages: any = [];
+  socket: any;
+  texto: string = '';
 
+  ngOnInit(): void {
+    this.socket = io(environment.apiUrl);
+
+    this.socket.on('newMessage', (data: any) => {
+      console.log('Alguien envio un mensaje');
+      this.messages.push(data)
+    })
+  }
+
+  sendNewMessage(){
+    console.log('Enviar un mensaje');
+    this.socket.emit('sendMessage', this.texto);
+    this.messages.push({
+      message: this.texto,
+      owned: true
+    })
+    this.texto = '';
+  }
 }
