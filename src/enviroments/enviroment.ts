c71086869eab925fd071479eeadc4cd807fa767f
Pablo Vergara <vergarapablo2001@gmail.com>
2023-05-12 20:00:07 -0600

    mejoras en enviroment

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,5 +1,4 @@
 export const environment = {
     apiUrl: 'http://localhost:3000/',
-    urlNewUser: 'http://localhost:3000/users/',
     googleId: '261052133252-nr3cj2ofsgmkuavml18ae0160jn4iret.apps.googleusercontent.com'
 };
