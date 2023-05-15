c65ff8532df36a9fb331c38e67458771fb725f1e
Pablo Vergara <vergarapablo2001@gmail.com>
2023-05-15 00:07:01 -0600

    listo para main todo estetico falta perfil

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiUrl: 'http://localhost:3000/',
+    apiUrl: 'https://uwoof.herokuapp.com/',
     googleId: '261052133252-nr3cj2ofsgmkuavml18ae0160jn4iret.apps.googleusercontent.com'
 };
